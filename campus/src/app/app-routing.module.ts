1c7f044bff353fee3e10b26097a8a515f17978fa
DongNyoen <qwert884@naver.com>
2024-08-19 00:00:19 +0900

    feat : sidemenu 컴포넌트 추가

--- campus/src/app/app-routing.module.ts
+++ campus/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SidemenuComponent } from './sidemenu/sidemenu.component'; // 사이드 메뉴 컴포넌트
 
 const routes: Routes = [
   {
@@ -10,7 +11,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: 'sidemenu', component: SidemenuComponent 
+  }, // 사이드 메뉴 경로
 ];
 
 @NgModule({
