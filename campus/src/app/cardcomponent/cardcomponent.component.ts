630101fe5db4f76ca851faea29314c8af94a7aa3
DongNyoen <qwert884@naver.com>
2024-08-21 18:38:45 +0900

    feat : cardcomponent추가

---
+++ campus/src/app/cardcomponent/cardcomponent.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardcomponent',
+  templateUrl: './cardcomponent.component.html',
+  styleUrls: ['./cardcomponent.component.scss'],
+})
+export class CardcomponentComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
