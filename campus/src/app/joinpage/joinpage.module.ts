c08ca4a0fe6f342b88dd651ab028cad3bf4f8921
yongchaeyoung <happy7yong83@gmail.com>
2024-08-21 10:31:42 +0900

    doc : 회원가입 페이지 생성

---
+++ campus/src/app/joinpage/joinpage.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { JoinpagePageRoutingModule } from './joinpage-routing.module';
+
+import { JoinpagePage } from './joinpage.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    JoinpagePageRoutingModule
+  ],
+  declarations: [JoinpagePage]
+})
+export class JoinpagePageModule {}
---
+++ campus/src/app/joinpage/joinpage.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joinpage',
+  templateUrl: './joinpage.page.html',
+  styleUrls: ['./joinpage.page.scss'],
+})
+export class JoinpagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
