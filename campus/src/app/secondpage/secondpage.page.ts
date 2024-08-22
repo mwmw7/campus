f0c25b0925aeacd02a3b350a54305b10c330f743
DongNyoen <qwert884@naver.com>
2024-08-22 16:54:10 +0900

    feat : jpg추가, 가로 정렬 성공

--- campus/src/app/secondpage/secondpage.page.ts
+++ campus/src/app/secondpage/secondpage.page.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { CardComponent } from '../cardcomponent/cardcomponent.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-secondpage',
   templateUrl: './secondpage.page.html',
   styleUrls: ['./secondpage.page.scss'],
 })
-export class SecondpagePage implements OnInit {
+export class SecondpagePage  {
   cards = [
     {
       title: '전시 카드 제목 1',
       teamname: '팀 이름 1',
       content: '카드 내용 설명 1입니다.',
-      image: 'https://ionicframework.com/docs/img/demos/card-media.png'
+      image: "../../assets/jpg/1.jpg"
     },
     {
       title: '전시 카드 제목 2',
       teamname: '팀 이름 2',
-      content: '카드 내용 설명 2입니dksfhkjsdhfakjsldhfdkjlsahfkljsdahfkjldshfkjahfkjshdfjhdkjslahfjkdshafkjsdhafjkhafdjkshjklfahskfhjshfaskjdlhfjdshaflkhdsjafhlkfhdsajfhjdksahfkjsadhfjfladshfjksadhflk다.',
-      image: 'https://ionicframework.com/docs/img/demos/card-media.png'
+      content: '카드 내용 설명 3입니다.',
+      image: "../../assets/jpg/2.jpg"
     },
     {
       title: '전시 카드 제목 3',
       teamname: '팀 이름 3',
       content: '카드 내용 설명 3입니다.',
-      image: 'https://ionicframework.com/docs/img/demos/card-media.png'
-    }
+      image: "../../assets/jpg/3.jpg"
+    },
+    {
+      title: '전시 카드 제목 4',
+      teamname: '팀 이름 4',
+      content: '카드 내용 설명 4입니다.',
+      image: "../../assets/jpg/4.jpg"
+    },
+
+
   ];
 
   accordionTitle: string = '최신순';
@@ -37,7 +44,6 @@ export class SecondpagePage implements OnInit {
     this.isOpen = !this.isOpen; // 아코디언 열림/닫힘 상태 반전
   }
 
-  ngOnInit() {
-  }
+
 
 }
