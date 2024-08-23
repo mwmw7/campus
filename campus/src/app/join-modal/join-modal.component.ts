312e3f598d6e1a1090c4eed19c699dc97fc2d179
yongchaeyoung <happy7yong83@gmail.com>
2024-08-23 18:55:35 +0900

    docs : 회원가입 모달탭 파일 생성

---
+++ campus/src/app/join-modal/join-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-join-modal',
+  templateUrl: './join-modal.component.html',
+  styleUrls: ['./join-modal.component.scss'],
+})
+export class JoinModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
