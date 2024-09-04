004840146357aaf9adc1ee34e5b0b988893ff6a2
dongnyoen <qwert884@naver.com>
2024-09-04 16:31:20 +0900

    feat : studyroommain 출첵체크 부분 중앙정렬

--- campus/src/app/studyroom/studyroom.page.ts
+++ campus/src/app/studyroom/studyroom.page.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-studyroom',
   templateUrl: './studyroom.page.html',
   styleUrls: ['./studyroom.page.scss'],
 })
-export class StudyroomPage implements OnInit {
-  currentDate: string;
+export class StudyroomPage implements OnInit, OnDestroy {
+  currentDate: string | undefined;
+  private intervalId: any;
 
   constructor() {
+    this.updateTime();
+  }
+
+  ngOnInit() {
+    // 1분마다 시간을 업데이트
+    this.intervalId = setInterval(() => {
+      this.updateTime();
+    }, 60000); // 60,000 ms = 1분
+  }
+
+  ngOnDestroy() {
+    // 컴포넌트가 파괴될 때 interval을 정리
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  private updateTime() {
     const now = new Date();
     this.currentDate = `${now.getFullYear()}년 ${now.getMonth() + 1}월 ${now.getDate()}일 ${now.getHours()}시 ${now.getMinutes()}분`;
   }
-
-
-  ngOnInit() {
-  }
-
 }
