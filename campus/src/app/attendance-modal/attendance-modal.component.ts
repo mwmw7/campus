709486f2e8d6d3d7af6fe03456e5ab0f3d533ae5
dongnyoen <qwert884@naver.com>
2024-09-07 10:44:06 +0900

    feat : attendance-modal 생성

---
+++ campus/src/app/attendance-modal/attendance-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance-modal',
+  templateUrl: './attendance-modal.component.html',
+  styleUrls: ['./attendance-modal.component.scss'],
+})
+export class AttendanceModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
