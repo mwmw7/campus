49191f5c7c32235db00c69751335f2ac35b680ab
yongchaeyoung <happy7yong83@gmail.com>
2024-09-21 08:08:53 +0900

    fix : 내클래스 페이지 css 수정 및 보편적 화면 생성

--- campus/src/app/joinpage/joinpage.page.ts
+++ campus/src/app/joinpage/joinpage.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AlertController } from '@ionic/angular';
+import { Component, OnInit,  } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,AbstractControl} from '@angular/forms';
+import { AlertController} from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
 import { Router } from "@angular/router";
 
@@ -12,6 +12,7 @@ import { Router } from "@angular/router";
 export class JoinpagePage implements OnInit {
   registerForm!: FormGroup;
   userRole: string | undefined;
+  showPassword = false;
 
   constructor(
     private fb: FormBuilder,
@@ -27,16 +28,36 @@ export class JoinpagePage implements OnInit {
     // 폼 초기화
     this.registerForm = this.fb.group({
       user_name: ['', Validators.required],
-      nick_name: ['', Validators.required],
+      nick_name: ['', Validators.required,Validators.minLength(3), Validators.maxLength(10)],
       user_role: [this.userRole || '', Validators.required], // 전달된 값 또는 빈 문자열로 초기화
-      id: ['', Validators.required],
-      password: ['', Validators.required],
+      id: ['', Validators.required, Validators.minLength(5), Validators.maxLength(20)],
+      password: ['', Validators.required,Validators.minLength(7), Validators.maxLength(20)],
+      passwordConfirm: ['', Validators.required], // 비밀번호 확인 필드 추가
       email: ['', [Validators.required, Validators.email]],
-      generation: ['', Validators.required]
     });
   }
 
+  // 비밀번호와 비밀번호 확인 필드가 일치하는지 확인하는 커스텀 유효성 검사
+  passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get('password')?.value;
+    const passwordConfirm = control.get('passwordConfirm')?.value;
+
+    if (password && passwordConfirm && password !== passwordConfirm) {
+      return { 'passwordMismatch': true };
+    }
+    return null;
+  }
+
+
+  // 비밀번호 보기 토글 메서드
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+
+
   async onSubmit() {
+
     if (this.registerForm.valid) {
       const data = this.registerForm.value;
 
