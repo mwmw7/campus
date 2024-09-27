c88508bab079ae6e768616c116555974fa157c93
yongchaeyoung <happy7yong83@gmail.com>
2024-09-27 23:30:12 +0900

    fix : 로그인 기능 / 회원가입 페이지 수정 / 클래스 페이지 수정

--- campus/src/app/services/auth.service.ts
+++ campus/src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { tap } from 'rxjs/operators';
 export class AuthService {
   private loggedIn = new BehaviorSubject<boolean>(false);
   isLoggedIn = this.loggedIn.asObservable();
-  private apiUrl = 'http://localhost:3000/users';
+  private apiUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {
     // 초기 로그인 상태 설정
@@ -18,16 +18,19 @@ export class AuthService {
 
   private checkInitialLoginStatus() {
     // 로컬 스토리지에 토큰이 있으면 로그인 상태를 true로 설정
+    console.log('true');
     this.loggedIn.next(!!localStorage.getItem('token'));
   }
 
   login_current(token: string) {
-    // 토큰을 로컬 스토리지에 저장하고 로그인 상태를 true로 설정
+    console.log('토큰 저장 및 로그인 상태 업데이트:', token);  // 디버깅용 로그
     localStorage.setItem('token', token);
-    this.loggedIn.next(true);
+    this.loggedIn.next(true);  // 로그인 상태를 true로 변경
   }
 
+
   logout_current() {
+    alert('false');
     // 로컬 스토리지에서 토큰을 제거하고 로그인 상태를 false로 설정
     localStorage.removeItem('token');
     this.loggedIn.next(false);
@@ -35,17 +38,19 @@ export class AuthService {
 
   // 사용자 등록 메서드
   register(userData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, userData);
+    return this.http.post(`${this.apiUrl}/users/register`, userData);
   }
 
   // 사용자 로그인 메서드
   login(credentials: any): Observable<any> {
-    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, credentials, { withCredentials: true })
+    return this.http.post<{ token: string }>(`${this.apiUrl}/auth/login`, credentials)
       .pipe(
         tap(response => {
-          // 로그인 성공 시 토큰을 저장하고 로그인 상태를 true로 설정
-          this.login_current(response.token);
+          console.log('로그인 응답:', response);
+          this.login_current(response.token); // 토큰 저장 및 상태 업데이트
         })
       );
   }
+
+
 }
